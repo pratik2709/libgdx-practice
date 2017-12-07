6ef4c260de39095d213560e62e4fd351a3e81a4e
pratiksaxena <pratik2709@gmail.com>
2017-12-08 00:58:36 +0530

    added walk loop animation and explosion animation

--- core/src/com/pratik/test/Animations.java
+++ core/src/com/pratik/test/Animations.java
@@ -1,10 +1,16 @@
 package com.pratik.test;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.DelayedRemovalArray;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import static com.badlogic.gdx.graphics.g2d.Animation.PlayMode.LOOP_PINGPONG;
+import static com.badlogic.gdx.graphics.g2d.Animation.PlayMode.NORMAL;
 
 public class Animations extends ApplicationAdapter{
     private static final float EXPLOSION_SPAWN_RATE = 20;
@@ -26,5 +32,30 @@ public class Animations extends ApplicationAdapter{
     public void create(){
         batch = new SpriteBatch();
         extendViewport = new ExtendViewport(100,100);
+
+        //set start time
+        startTime = System.nanoTime();
+
+        Array<TextureRegion> walkLoopTextures = new Array<TextureRegion>();
+
+        //add texture region to arrays
+        walkLoopTextures.add(new TextureRegion(new Texture("walk-1-right.png")));
+        walkLoopTextures.add(new TextureRegion(new Texture("walk-2-right.png")));
+        walkLoopTextures.add(new TextureRegion(new Texture("walk-3-right.png")));
+
+        //add walkloop animation
+        walkLoop = new Animation(WALK_LOOP_FRAME_DURATION, walkLoopTextures ,LOOP_PINGPONG);
+
+        Array<TextureRegion> explosionTextures = new Array<TextureRegion>();
+        explosionTextures.add(new TextureRegion(new Texture("explosion-large.png")));
+        explosionTextures.add(new TextureRegion(new Texture("explosion-medium.png")));
+        explosionTextures.add(new TextureRegion(new Texture("explosion-small.png")));
+        explosion = new Animation(EXPLOSION_FRAME_DURATION, explosionTextures, NORMAL);
+        explosions = new DelayedRemovalArray<OneShotAnimation>();
+    }
+
+    @Override
+    public void resize(){
+
     }
 }
