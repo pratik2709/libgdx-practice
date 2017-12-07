ba2e8e1c0228af6930ef87c5400fb0503127daf1
pratiksaxena <pratik2709@gmail.com>
2017-12-08 01:37:30 +0530

    added OneShotAnimation class

--- core/src/com/pratik/test/OneShotAnimation.java
+++ core/src/com/pratik/test/OneShotAnimation.java
@@ -1,5 +1,32 @@
 package com.pratik.test;
 
-public class OneShotAnimation {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
 
+public class OneShotAnimation {
+    public final Vector2 position;
+    private final Animation animation;
+    private final long startTimeNanos;
+
+    public OneShotAnimation(Animation animation, Vector2 position, long startTimeNanos){
+        this.animation = animation;
+        this.position = position;
+        this.startTimeNanos = startTimeNanos;
+    }
+
+    private float elapsedTime(){
+        return MathUtils.nanoToSec * (TimeUtils.nanoTime() - startTimeNanos);
+    }
+
+    public TextureRegion getFrame(){
+        return animation.getKeyFrame(elapsedTime());
+    }
+
+    public boolean isAnimationFinished(){
+        return animation.isAnimationFinished(elapsedTime());
+    }
 }
