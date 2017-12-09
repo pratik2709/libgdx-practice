027d5e1850991a900189df77a29ec718f252d3ed
pratiksaxena <pratik2709@gmail.com>
2017-12-09 14:46:36 +0530

    added gigagal class

--- desktop/src/com/pratik/test/desktop/DesktopLauncher.java
+++ desktop/src/com/pratik/test/desktop/DesktopLauncher.java
@@ -5,10 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pratik.test.Animations;
 import com.pratik.test.NinePatchDemo;
 import com.pratik.test.TestGame;
+import com.udacity.gamedev.gigagal.GigaGalGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Animations(), config);
+		new LwjglApplication(new GigaGalGame(), config);
 	}
 }
