ea84b2092c3c91c57c5e0d96bce63f68f550ee2f
pratiksaxena <pratik2709@gmail.com>
2017-12-10 14:46:31 +0530

    added some constants

--- core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -1,4 +1,20 @@
 package com.udacity.gamedev.gigagal.util;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
 public class Constants {
+    //background color
+    public static final Color BACKGROUND_COLOR = Color.SKY;
+
+
+    public static final float WORLD_SIZE = 128;
+
+    public static final String TEXTURE_ATLAS = "images/gigagal.pack.atlas";
+
+    //constant for standing right
+    public static final String STANDING_RIGHT = "standing-right";
+
 }
