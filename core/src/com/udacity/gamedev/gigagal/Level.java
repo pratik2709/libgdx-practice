0ed43f2687b00c5356f0c666bdc319d28e4d9d4d
pratiksaxena <pratik2709@gmail.com>
2017-12-10 23:07:39 +0530

    added level code which calls the gigagal entity

--- core/src/com/udacity/gamedev/gigagal/Level.java
+++ core/src/com/udacity/gamedev/gigagal/Level.java
@@ -1,18 +1,20 @@
 package com.udacity.gamedev.gigagal;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.udacity.gamedev.gigagal.entities.GigaGal;
 
 public class Level {
+    GigaGal gigaGal;
 
     public Level(){
-
+        gigaGal = new GigaGal();
     }
 
     public void render(SpriteBatch batch){
-
+        gigaGal.render(batch);
     }
 
     public void update(float delta){
-
+        gigaGal.update(delta);
     }
 }
