42fac994c8ca51e318d4e387838d15cd0fae73a4
pratiksaxena <pratik2709@gmail.com>
2017-12-31 23:06:52 +0530

    finished explosions

--- core/src/com/udacity/gamedev/gigagal/util/ExplosionAssets.java
+++ core/src/com/udacity/gamedev/gigagal/util/ExplosionAssets.java
@@ -1,9 +1,24 @@
 package com.udacity.gamedev.gigagal.util;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 
 public class ExplosionAssets {
 
+    Animation<TextureRegion> explosionAnimation;
+
     public ExplosionAssets(TextureAtlas atlas) {
+
+        Array<TextureAtlas.AtlasRegion> explosionFrames = new Array<TextureAtlas.AtlasRegion>();
+
+        explosionFrames.add(atlas.findRegion(Constants.EXPLOSION_LARGE));
+        explosionFrames.add(atlas.findRegion(Constants.EXPLOSION_SMALL));
+        explosionFrames.add(atlas.findRegion(Constants.EXPLOSION_MEDIUM));
+
+        explosionAnimation = new Animation<TextureRegion>(Constants.EXPLOSION_DURATION,
+                explosionFrames,
+                Animation.PlayMode.LOOP);
     }
 }
