ab24b7fca795a2dbd536c38e927402739dda0bb9
pratiksaxena <pratik2709@gmail.com>
2018-01-27 16:33:24 +0530

    added to code to shake the camera

--- core/src/com/pratik/libgdx/testios/util/ChaseCam.java
+++ core/src/com/pratik/libgdx/testios/util/ChaseCam.java
@@ -2,6 +2,8 @@ package com.pratik.libgdx.testios.util;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.pratik.libgdx.testios.entities.GigaGal;
 
 import static com.badlogic.gdx.Input.Keys.*;
@@ -10,6 +12,11 @@ public class ChaseCam {
     Camera camera;
     GigaGal gigaGalPlayer;
     Boolean following;
+
+    float radius = 30.0f;
+    float randomAngle = MathUtils.random()%360;
+//    Vector2 offset = new Vector2(MathUtils.sin(randomAngle) * radius, MathUtils.cos(randomAngle));
+    Vector2 offset = new Vector2(0, 0);
 
     public ChaseCam(Camera camera, GigaGal gigaGal) {
         this.camera = camera;
@@ -24,8 +31,10 @@ public class ChaseCam {
         }
 
         if(following){
-            camera.position.x = gigaGalPlayer.gigagalPosition.x;
-            camera.position.y = gigaGalPlayer.gigagalPosition.y;
+            camera.position.x = gigaGalPlayer.gigagalPosition.x + offset.x;
+            camera.position.y = gigaGalPlayer.gigagalPosition.y + offset.y;
+            //temporarty method for shaking
+            shakeCamera();
         }
         else{
             if(Gdx.input.isKeyPressed(A)){
@@ -40,7 +49,15 @@ public class ChaseCam {
             else if(Gdx.input.isKeyPressed(S)){
                 camera.position.y -= delta * Constants.CAMERA_MOVE_SPEED;
             }
+
         }
 
+    }
+
+    private Vector2 shakeCamera() {
+        radius *= 0.9;
+        randomAngle += (150 + MathUtils.random()%60);
+        offset = new Vector2(MathUtils.sin(randomAngle) * radius, MathUtils.cos(randomAngle));
+        return offset;
     }
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+Shaking camera:
+https://gamedev.stackexchange.com/questions/1828/realistic-camera-screen-shake-from-explosion/1833#1833
