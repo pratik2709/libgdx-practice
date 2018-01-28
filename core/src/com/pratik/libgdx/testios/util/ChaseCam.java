3c7cfd54f27fdef6b5d382273b9fcc5812a00135
pratiksaxena <pratik2709@gmail.com>
2018-01-28 17:21:11 +0530

    added start shake and stop methods

--- core/src/com/pratik/libgdx/testios/util/ChaseCam.java
+++ core/src/com/pratik/libgdx/testios/util/ChaseCam.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.pratik.libgdx.testios.entities.GigaGal;
 
 import static com.badlogic.gdx.Input.Keys.*;
@@ -12,41 +13,42 @@ public class ChaseCam {
     Camera camera;
     GigaGal gigaGalPlayer;
     Boolean following;
+    public Boolean setChasecam;
+    public long shakeStartTime;
+    public Enums.shakeState shakeState;
+    int startTick = 0;
 
     float radius = 30.0f;
-    float randomAngle = MathUtils.random()%360;
-//    Vector2 offset = new Vector2(MathUtils.sin(randomAngle) * radius, MathUtils.cos(randomAngle));
-    Vector2 offset = new Vector2(0, 0);
+    float randomAngle = MathUtils.random() % 360;
+    //    Vector2 offset = new Vector2(MathUtils.sin(randomAngle) * radius, MathUtils.cos(randomAngle));
+    public Vector2 offset = new Vector2(0, 0);
 
     public ChaseCam(Camera camera, GigaGal gigaGal) {
         this.camera = camera;
         this.gigaGalPlayer = gigaGal;
         following = true;
+        setChasecam = false;
+        shakeStartTime = 0;
+        shakeState = Enums.shakeState.STOP;
     }
 
     public void update(float delta) {
 
         if (Gdx.input.isKeyPressed(SPACE)) {
-            following = ! following;
+            following = !following;
         }
 
-        if(following){
+        if (following) {
             camera.position.x = gigaGalPlayer.gigagalPosition.x + offset.x;
             camera.position.y = gigaGalPlayer.gigagalPosition.y + offset.y;
-            //temporarty method for shaking
-//            shakeCamera();
-        }
-        else{
-            if(Gdx.input.isKeyPressed(A)){
+        } else {
+            if (Gdx.input.isKeyPressed(A)) {
                 camera.position.x -= delta * Constants.CAMERA_MOVE_SPEED;
-            }
-            else if(Gdx.input.isKeyPressed(D)){
+            } else if (Gdx.input.isKeyPressed(D)) {
                 camera.position.x += delta * Constants.CAMERA_MOVE_SPEED;
-            }
-            else if(Gdx.input.isKeyPressed(W)){
+            } else if (Gdx.input.isKeyPressed(W)) {
                 camera.position.y += delta * Constants.CAMERA_MOVE_SPEED;
-            }
-            else if(Gdx.input.isKeyPressed(S)){
+            } else if (Gdx.input.isKeyPressed(S)) {
                 camera.position.y -= delta * Constants.CAMERA_MOVE_SPEED;
             }
 
@@ -54,10 +56,36 @@ public class ChaseCam {
 
     }
 
-    private Vector2 shakeCamera() {
+    public Vector2 shakeCamera() {
         radius *= 0.9;
-        randomAngle += (150 + MathUtils.random()%60);
+        randomAngle += (180 + MathUtils.random() % 60);
         offset = new Vector2(MathUtils.sin(randomAngle) * radius, MathUtils.cos(randomAngle));
         return offset;
+    }
+
+    public void startShakeCam() {
+        if (shakeState != Enums.shakeState.START && shakeState == Enums.shakeState.NEXT_READY) {
+            shakeState = Enums.shakeState.START;
+            shakeStartTime = TimeUtils.nanoTime();
+        }
+        if(shakeState == Enums.shakeState.START){
+            shakeCamera();
+        }
+    }
+
+    public void stopShakeCam() {
+        float elapsed = Util.secondsSince(shakeStartTime);
+        System.out.println(elapsed + ":::" + Constants.SHAKE_DURATION);
+        System.out.println(elapsed > Constants.SHAKE_DURATION);
+
+        if (elapsed > Constants.SHAKE_DURATION) {
+            offset.setZero();
+            shakeStartTime = 0;
+            shakeState = Enums.shakeState.STOP;
+        }
+        //shaking needs to stop here until a jump happens
+        // take a break until next trigger?
+        //whats the trigger ?
+        //
     }
 }
